1f34bd5e099473c7103096b0cbe1b48e582ed1e9
bestpvp <bestpvp@sina.com>
2024-07-30 19:43:13 +0800

    fix

--- app/src/main/java/com/github/catvod/spider/TvDy.java
+++ app/src/main/java/com/github/catvod/spider/TvDy.java
@@ -45,7 +45,7 @@ public class TvDy extends Spider {
             classes.add(new Class(typeIdList[i], typeNameList[i]));
         }
         Document doc = Jsoup.parse(OkHttp.string(siteUrl, getHeaders()));
-        for (Element element : doc.select("div.stui-vodlist__box a")) {
+        for (Element element : doc.select("div.stui-vodlist__box a.stui-vodlist__thumb.lazyload")) {
             try {
                 String pic = element.attr("data-original");
                 String url = element.attr("href");
